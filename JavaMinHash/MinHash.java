a8b361c89f496782c2a49fb1a3997a5b336494e6
Mateus Cruz <mshcruz@gmail.com>
2013-11-27 11:42:04 +0900

    Removing unecessary parts

--- JavaMinHash/MinHash.java
+++ JavaMinHash/MinHash.java
@@ -1,3 +1,5 @@
+//Modifications made based on the code at http://mymagnadata.wordpress.com/2011/01/04/minhash-java-implementation/ 
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -9,15 +11,11 @@ public class MinHash<T> {
     private int hash[][];
     private int numHash;
     
-    /**
-     * 
-     */
     public MinHash(int numHash){
 	this.numHash = numHash;
-
-	System.out.println("numHash: "+numHash);
     }
-    
+
+    /*    
     private void printMinHashValues(int[][] minHashValues){
 	for(int i=0; i < 2; i++){
 	    for(int j=0; j<numHash; j++){
@@ -26,16 +24,14 @@ public class MinHash<T> {
 	    System.out.println();
 	}
     }
+    */
 
     public double similarity(Set<T> set1, Set<T> set2){
-
         int numSets = 2;
         Map<T, boolean[]> bitMap = buildBitMap(set1, set2);
         hash = new int[bitMap.size()][numHash];        
 	
 	Random r = new Random(11);
-
-
 	for (int j = 0; j < bitMap.size(); j++){
 	    for (int i = 0; i < numHash; i++){
 		int a = (int)r.nextInt();
@@ -43,27 +39,15 @@ public class MinHash<T> {
 		int c = (int)r.nextInt();
 		int x = hash(a*b*c, a, b, c);
 		hash[j][i] = x;
-		//		System.out.print(hash[j][i]+" ");
 	    }  
-	    //	    System.out.println();
-	    //	    System.out.println();
         } 
 
         int[][] minHashValues = initializeHashBuckets(numSets, numHash);
-	//	printMinHashValues(minHashValues);
-	//	System.out.println();
         computeMinHashForSet(set1, 0, minHashValues, bitMap);
-	//	printMinHashValues(minHashValues);
-	//	System.out.println();
         computeMinHashForSet(set2, 1, minHashValues, bitMap);
-	//	printMinHashValues(minHashValues);
-	//	System.out.println();
         return computeSimilarityFromSignatures(minHashValues, numHash);
     }
     
-    /**
-     * 
-vv     */
     private static int[][] initializeHashBuckets(int numSets, int numHashFunctions) {
 	int[][] minHashValues = new int[numSets][numHashFunctions];
 
@@ -75,12 +59,6 @@ vv     */
         return minHashValues;
     }
      
-    /**
-     * 
-     * @param minHashValues
-     * @param numHashFunctions
-     * @return
-     */
     private static double computeSimilarityFromSignatures(int[][] minHashValues, int numHashFunctions) {
 	int identicalMinHashes = 0;
         for (int i = 0; i < numHashFunctions; i++){
@@ -88,18 +66,9 @@ vv     */
                 identicalMinHashes++;
             }
         }
-	System.out.println("IdenticalMinhashes: "+identicalMinHashes);
         return (1.0 * identicalMinHashes) / numHashFunctions;
     }
     
-    /**
-     * 
-     * @param x
-     * @param a
-     * @param b
-     * @param c
-     * @return
-     */
     private static int hash(int x, int a, int b, int c) {
         int hashValue = (int)((a * (x >> 4) + b * x + c) & 131071);
         return Math.abs(hashValue);
@@ -123,12 +92,6 @@ vv     */
 	}
     }
     
-    /**
-     * 
-     * @param set1
-     * @param set2
-     * @return
-     */
     public Map<T,boolean[]> buildBitMap(Set<T> set1, Set<T> set2){
 	
 	Map<T,boolean[]> bitArray = new HashMap<T,boolean[]>();
@@ -178,22 +141,23 @@ vv     */
 	set4.add("g");
 	set4.add("k");
 
-	MinHash<String> minHash = new MinHash<String>(1000);
-	System.out.println("Similarity S1 and S2: "+minHash.similarity(set1, set2)+" Real similarity: 0.28");
+	//Raising the permutations number increases the accuracy
+	MinHash<String> minHash = new MinHash<String>(256);
+	System.out.println("Similarity S1 and S2: "+minHash.similarity(set1, set2)+" - Real similarity: 0.28");
 
-	MinHash<String> minHash2 = new MinHash<String>(1000);
-	System.out.println("Similarity S1 and S3: "+minHash2.similarity(set1, set3)+" Real similarity: 0.14");
+	MinHash<String> minHash2 = new MinHash<String>(256);
+	System.out.println("Similarity S1 and S3: "+minHash2.similarity(set1, set3)+" - Real similarity: 0.14");
 
-	MinHash<String> minHash3 = new MinHash<String>(1000);
-	System.out.println("Similarity S1 and S4: "+minHash3.similarity(set1, set4)+" Real similarity: 0.66");
+	MinHash<String> minHash3 = new MinHash<String>(256);
+	System.out.println("Similarity S1 and S4: "+minHash3.similarity(set1, set4)+" - Real similarity: 0.66");
 
-	MinHash<String> minHash4 = new MinHash<String>(1000);
-	System.out.println("Similarity S2 and S3: "+minHash4.similarity(set2, set3)+" Real similarity: 0.28");
+	MinHash<String> minHash4 = new MinHash<String>(256);
+	System.out.println("Similarity S2 and S3: "+minHash4.similarity(set2, set3)+" - Real similarity: 0.28");
 
-	MinHash<String> minHash5 = new MinHash<String>(1000);
-	System.out.println("Similarity S2 and S4: "+minHash5.similarity(set2, set4)+" Real similarity: 0.42");
+	MinHash<String> minHash5 = new MinHash<String>(256);
+	System.out.println("Similarity S2 and S4: "+minHash5.similarity(set2, set4)+" - Real similarity: 0.42");
 	
-	MinHash<String> minHash6 = new MinHash<String>(1000);
-	System.out.println("Similarity S3 and S4: "+minHash6.similarity(set3, set4)+" Real similarity: 0.11");
+	MinHash<String> minHash6 = new MinHash<String>(256);
+	System.out.println("Similarity S3 and S4: "+minHash6.similarity(set3, set4)+" - Real similarity: 0.11");
     }
 }
