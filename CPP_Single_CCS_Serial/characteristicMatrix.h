d6ebe8ef4d42a949fdbe2d94d00ed70b53d200c9
Mateus Cruz <mshcruz@gmail.com>
2014-06-09 20:22:50 +0900

    Adding CPP Single CCS Serial

---
+++ CPP_Single_CCS_Serial/characteristicMatrix.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <map>
+#include <cstddef>
+
+typedef struct ccsMatrix ccsMatrix;
+struct ccsMatrix{
+  std::vector<int> row_ind;
+  std::vector<int> col_ptr;
+};
+
+ccsMatrix* buildCharacteristicMatrix(std::multimap<int,int>);
+void printCharacteristicMatrixVectors (ccsMatrix*);
+int ccsMatrixSize (ccsMatrix*);
